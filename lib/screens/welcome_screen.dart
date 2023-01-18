7a9f961f6800772dc0db84290a0cf1c6508ff5a0
Hosna <HosnaAazem5@gmail.com>
2023-01-18 16:57:12 +0500

    Third commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,129 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat_starting_project/screens/login_screen.dart';
+import 'package:flash_chat_starting_project/screens/registration_screen.dart';
+
+import '/constants.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+
+    animation =  ColorTween(
+      begin: Colors.yellow.shade800,
+      end: Colors.grey[900],
+    ).animate(controller);
+    controller.forward();
+
+   
+    controller.addListener(() {
+      print(animation.value);
+      setState(() {});
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.amber.withOpacity(controller.value),
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    height: 60,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
+                  ),
+                  child: AnimatedTextKit(
+                    totalRepeatCount: 12,
+                    animatedTexts: [
+                    TypewriterAnimatedText('Flash Chat')
+                
+                  ]),
+                ),
+               
+              ],
+            ),
+            const SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Material(
+                elevation: 5.0,
+                color: kLoginButtonColor,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: const Text(
+                    'Log In',
+                    style: TextStyle(color: kWhiteColor),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Material(
+                elevation: 5.0,
+                color: kRegisterButtonColor,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    //Go to registration screen
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(color: kWhiteColor),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
